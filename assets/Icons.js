97b1791a357531ffcb7da5f8b5a008d9f412ba59
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-06-29 21:52:32 -0700

    sidebar

--- assets/Icons.js
+++ assets/Icons.js
@@ -1,6 +1,24 @@
-import { Bell, UserCircle } from 'lucide-react';
+import {
+    Bell, Plus, UserCircle, Blocks,
+    PanelLeft,
+    Mail,
+    Power,
+    BookOpen,
+    Settings,
+    Chrome,
+    Headset,
+} from 'lucide-react';
 
 export const Icons = {
     UserCircle,
-    Bell
+    Bell,
+    Plus,
+    Headset,
+    Chrome,
+    Settings,
+    BookOpen,
+    Mail,
+    Power,
+    PanelLeft,
+    Blocks
 }
