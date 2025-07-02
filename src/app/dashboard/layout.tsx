3e61219803c5601a8a17e6dee90c79161c20c077
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-03 01:03:25 +0500

    Remove unnecessary whitespace in layout component for cleaner code.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='bg-[#111113]  p-6'>
-        
-        {children}
+      {children}
     </div>
   )
 }
