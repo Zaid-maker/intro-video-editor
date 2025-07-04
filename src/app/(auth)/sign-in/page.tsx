24e1b75e10a140b2d1dbf36a426ea7e20345ace6
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-04 22:55:32 +0500

    Add authentication pages and SignInView component with form handling

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,20 @@
+import SignInView from '@/components/views/SignInView'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!!session) {
+        redirect('/dashboard')
+    }
+
+    return (
+        <SignInView />
+    )
+}
+
+export default Page
