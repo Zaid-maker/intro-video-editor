dee58a7dda15b1a13cf31e8690baaf857db7fc36
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-06 22:30:55 +0500

    style: remove unnecessary whitespace in main tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Footer } from "./(landing)/components/Footer";
 
 export default function Home() {
   return (
-    <main className=" bg-[#111113]    ">
+    <main className="bg-[#111113]">
       <Hero />
 
       <Footer />
