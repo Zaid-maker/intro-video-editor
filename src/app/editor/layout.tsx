25eab336ae9533eca1fec72bbabb493989d02bf7
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-07 16:05:44 +0500

    protect editor route aswell

---
+++ src/app/editor/layout.tsx
@@ -0,0 +1,17 @@
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) redirect("/sign-in");
+
+    return (
+        <div className='bg-[#111113] p-6 w-screen h-screen overflow-hidden'>
+            {children}
+        </div>
+    );
+}
