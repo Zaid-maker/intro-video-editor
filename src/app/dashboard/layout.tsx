4fc91405db78162562dfc6272e30bad7786a3bcd
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-07 15:41:49 +0500

    enforce authentication of the pages that requires authentication and redirects them to signin page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,22 @@
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-function layout({ children }: { children: React.ReactNode }) {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function layout({ children }: LayoutProps) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) redirect("/sign-in");
+
   return (
     <div className='bg-[#111113]  p-6'>
       {children}
     </div>
   )
 }
-
-export default layout
