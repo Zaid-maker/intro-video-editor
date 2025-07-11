25c91a0f92f40a3a4dadcf52ea5ce279942008b8
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-07-11 23:26:42 +0500

    remove /intro

--- src/app/intro/page.tsx
+++
@@ -1,9 +0,0 @@
-import IntroClient from './IntroClient';
-
-// Server-side configuration
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-export default function IntroPage() {
-  return <IntroClient />;
-}
